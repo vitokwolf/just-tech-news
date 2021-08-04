e90bafb16418c3981a0dcc4d83c230b056424647
Victor Lupascu <sinajeen@gmail.com>
2021-08-03 21:28:02 -0500

    improve user experience

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,10 @@ router.get('/', (req, res) => {
             // console.log(dbPostData[0]);
             const posts = dbPostData.map(post => post.get({ plain: true }));
             // pass a single post object into the homepage template
-            res.render('homepage', { posts });
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
