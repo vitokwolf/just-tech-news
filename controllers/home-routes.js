42a94414b1c0f388fea99f8135f19b54c28e9b90
Victor Lupascu <sinajeen@gmail.com>
2021-08-03 21:24:27 -0500

    add comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -85,7 +85,10 @@ router.get('/post/:id', (req, res) => {
             const post = dbPostData.get({ plain: true });
 
             // pass data to template
-            res.render('single-post', { post });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
